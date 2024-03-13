e16ddaa0e6b49a661bec2f13650fa2f0fcf4e23b
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-03-13 17:18:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Description from "./components/Description/Description.jsx";
 import Feedback from "./components/Feedback/Feedback.jsx";
 import Options from "./components/Options/Options.jsx";
+import { useState } from "react";
 
 
 
@@ -15,6 +16,15 @@ const handleLogOptions = (optionName) => {
   console.log("optionName: ", optionName);
 
 };
+
+
+const [counter, setCounter]=useState(0);
+
+const handleIncrementCounter = () => {
+  setCounter (counter +1);
+  console.log(counter);
+}
+
 
   return (
     <>
