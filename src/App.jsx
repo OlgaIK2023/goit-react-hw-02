00a77e4bb7e49e61d6587c168873fa27306f83a1
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-03-14 17:13:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,29 @@ import Feedback from "./components/Feedback/Feedback.jsx";
 import Options from "./components/Options/Options.jsx";
 import { useState } from "react";
 
-
-
-
 //APP code below
 
-const App = () => {
+function App () {
+  const [counter, setCounter] = useState(0);
+  const [options, setOptions] = useState({ good: 0, bad: 0, neutral: 0 });
 
-  const initialFeedback = {good: 0, bad: 0, neutral: 0}
+  const handleLogOptions = (optionName) => {
+    console.log("optionName: ", optionName);
+    setOptions({...options, [optionName]: options[optionName] + 1 });
+  };
 
-const handleLogOptions = (optionName) => {
-  console.log("optionName: ", optionName);
-
-};
-
-
-const [counter, setCounter]=useState(0);
-
-const handleIncrementCounter = () => {
-  setCounter (counter +1);
-  
-}
-
+  const handleIncrementCounter = () => {
+    setCounter(counter + 1);
+  };
 
   return (
     <div>
-      <button onClick = {handleIncrementCounter}> Counter: {counter}</button>
+      <button onClick={handleIncrementCounter}> Counter: {counter}</button>
       <Description />
-      <Options handleLogOptions = {handleLogOptions}/>
-      <Feedback options = {initialFeedback}/>
-
-      
+      <Options handleLogOptions={handleLogOptions} />
+      <Feedback options={options} />
     </div>
   );
-};
+}
 
 export default App;
