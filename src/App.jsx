66bac328734da2dae286ecb5f760c3bda2f92f37
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-03-15 16:00:49 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Options from "./components/Options/Options.jsx";
 //APP code below
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  
   const [options, setOptions] = useState({ good: 0, bad: 0, neutral: 0 });
 
   const handleLogOptions = (optionName) => {
@@ -15,25 +15,17 @@ function App() {
     setOptions({ ...options, [optionName]: options[optionName] + 1 });
   };
 
-  const handleIncrementCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleDecrementCounter = () => {
-    if (counter === 0) return;
-
-    setCounter(counter - 1);
-  };
+  
 
   const optionsTotal = options.good + options.bad + options.neutral;
 
   return (
     <div>
-      <button onClick={handleIncrementCounter}>Counter: {counter}</button>
-      <button onClick={handleDecrementCounter}>-</button>
+      
       <Description />
-      <Feedback options={options} total={optionsTotal} />
+     
       <Options handleLogOptions={handleLogOptions} />
+      <Feedback options={options} total={optionsTotal} />
     </div>
   );
 }
