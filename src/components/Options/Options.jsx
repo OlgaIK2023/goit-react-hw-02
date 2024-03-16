2d4751034f50de17079e7650c2a61b78a63dc1de
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-03-16 14:50:18 +0200

    Update Options.jsx

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,34 +3,31 @@ import "./Options.module.css";
 
 // import clsx from "clsx"
 
+const options = ({
+  handleLogOptions,
+  handleReset,
+  onToggleFeedbackVisibility,
+  total,
+}) => {
+  useEffect(() => {
+    const onKeyDown = (event) => {
+      if (event.code === "Escape") {
+        onToggleFeedbackVisibility();
+      }
+    };
 
+    window.addEventListener("keydown", onKeyDown);
 
-const options = ({handleLogOptions, handleReset,
-  onToggleFeedbackVisibility, total}) => {
-
-
-
-    useEffect(() => {
-      const onKeyDown = (event) => {
-        if (event.code === "Escape") {
-          onToggleFeedbackVisibility();
-        }
-      };
-  
-      window.addEventListener("keydown", onKeyDown);
-  
-      return () => {
-        window.removeEventListener("keydown", onKeyDown);
-      };
-    }, [onToggleFeedbackVisibility]);
-
-
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onToggleFeedbackVisibility]);
 
   return (
     <div>
-      <button onClick = {() => handleLogOptions("good")}>Good</button>
-      <button onClick = {() => handleLogOptions("neutral")}>Neutral</button>
-      <button onClick = {() => handleLogOptions("bad")}>Bad</button>
+      <button onClick={() => handleLogOptions("good")}>Good</button>
+      <button onClick={() => handleLogOptions("neutral")}>Neutral</button>
+      <button onClick={() => handleLogOptions("bad")}>Bad</button>
       {total !== 0 && <button onClick={handleReset}>Reset</button>}
     </div>
   );
